7979644ce79880ce0339846760daa90c0191eb6e
Muhammad Ibrahim Khan <moh.ibrahimkhan.mike@gmail.com>
2022-02-22 10:45:34 +0500

    Create config for hyperparameters

--- src/sentiment/train.py
+++ src/sentiment/train.py
@@ -5,6 +5,7 @@ from keras import Sequential
 from keras.layers import Dense, Embedding, GlobalAveragePooling1D
 from keras.layers import TextVectorization
 
+import config_train
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -25,19 +26,9 @@ class Utils():
         return [x.label for x in self.tweets]
     
 
-# Setting Hyper-parameters
-BATCH_SIZE = 1024
-SEED = 123
-DENSE_NODES = 16
-OPTIMIZER = 'adam'
-METRICS = ['accuracy']
-EPOCHS = 5
-VOCAB_SIZE = 10000
-SEQUENCE_LEN = 50
-EMBEDDING_DIM = 16
-LOAD_MODEL = True
+# Hyperparameters located in config_train.py
 
-if LOAD_MODEL:
+if config_train.LOAD_MODEL:
     model = tf.keras.models.load_model('/home/mik/TSA/models/tf_word_em')
 
 else:
@@ -62,33 +53,34 @@ else:
 
     # Text Vectorization
     vectorize_layer = TextVectorization(standardize='lower_and_strip_punctuation',
-                                    max_tokens=VOCAB_SIZE,
+                                    max_tokens=config_train.VOCAB_SIZE,
                                     split='whitespace',
                                     output_mode='int',
-                                    output_sequence_length=SEQUENCE_LEN)
+                                    output_sequence_length=config_train.SEQUENCE_LEN)
 
     vectorize_layer.adapt(p_text)
 
     # Create Model
     model = Sequential([
         vectorize_layer,
-        Embedding(VOCAB_SIZE, EMBEDDING_DIM, name='embedding'),
+        Embedding(config_train.VOCAB_SIZE, config_train.EMBEDDING_DIM,
+         name='embedding'),
         GlobalAveragePooling1D(),
-        Dense(DENSE_NODES, activation='relu'),
+        Dense(config_train.DENSE_NODES, activation='relu'),
         Dense(1, activation='sigmoid') # We want either 0 or 1 for our sentiment analysis
     ])
 
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='logs') # Saving statistics for tensorboard
 
     # Compiling and training model
-    model.compile(optimizer=OPTIMIZER,
+    model.compile(optimizer=config_train.OPTIMIZER,
                 loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-                metrics=METRICS)
+                metrics=config_train.METRICS)
 
     model.fit(x=ds_text,
             y=ds_labels,
-            batch_size=BATCH_SIZE,
-            epochs=EPOCHS, 
+            batch_size=config_train.BATCH_SIZE,
+            epochs=config_train.EPOCHS, 
             validation_split=0.1,
             callbacks=[tensorboard_callback])
 
